64f7762a242a1c1538dc2e571194e345f6622e6a
gabrielk <mail@gabrielkovacs.net>
2019-10-30 15:05:43 +0100

    started with UC 6 showDeliveryReports

---
+++ showDeliveryReports/src/main/java/net/gabrielkovacs/showDeliveryReports/ShowDeliveryReportsApplication.java
@@ -0,0 +1,13 @@
+package net.gabrielkovacs.showDeliveryReports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowDeliveryReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowDeliveryReportsApplication.class, args);
+	}
+
+}
