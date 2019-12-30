d47319533be8da0d52ec2237eca188e6b65aea33
gabrielk <mail@gabrielkovacs.net>
2019-12-30 20:39:46 +0100

    updated start script

--- scripts/start_jars.sh
+++ scripts/start_jars.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-java -jar /home/ec2-user/deploy/orderProductsAndReceiveOrderedProducts-0.0.1-SNAPSHOT.jar 
-java -jar /home/ec2-user/deploy/showDeliveryReports-0.0.1-SNAPSHOT.jar 
-java -jar /home/ec2-user/deploy/showStockReports-0.0.1-SNAPSHOT.jar 
-echo "finished working on starting the jars"
+java -jar /home/ec2-user/deploy/orderProductsAndReceiveOrderedProducts-0.0.1-SNAPSHOT.jar & 
+java -jar /home/ec2-user/deploy/showDeliveryReports-0.0.1-SNAPSHOT.jar &
+java -jar /home/ec2-user/deploy/showStockReports-0.0.1-SNAPSHOT.jar &
