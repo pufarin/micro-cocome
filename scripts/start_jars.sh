47da5bfff6ec70857f38d4b4ab679c9ed4e48adc
gabrielk <mail@gabrielkovacs.net>
2019-12-30 15:28:34 +0100

    start_jars.sh added absolute path

--- scripts/start_jars.sh
+++ scripts/start_jars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nohup java -jar orderProductsAndReceiveOrderedProducts-0.0.1-SNAPSHOT.jar &
-nohup java -jar showDeliveryReports-0.0.1-SNAPSHOT.jar &
-nohup java -jar showStockReports-0.0.1-SNAPSHOT.jar &
+nohup java -jar /home/ec2-user/deploy/orderProductsAndReceiveOrderedProducts-0.0.1-SNAPSHOT.jar &
+nohup java -jar /home/ec2-user/deploy/showDeliveryReports-0.0.1-SNAPSHOT.jar &
+nohup java -jar /home/ec2-user/deploy/showStockReports-0.0.1-SNAPSHOT.jar &
 
