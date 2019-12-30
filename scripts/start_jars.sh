18fb9966aad52e7031d79bb4732b397602e4e330
gabrielk <mail@gabrielkovacs.net>
2019-12-30 18:20:35 +0100

    updated start_jars

--- scripts/start_jars.sh
+++ scripts/start_jars.sh
@@ -2,4 +2,5 @@
 nohup java -jar /home/ec2-user/deploy/orderProductsAndReceiveOrderedProducts-0.0.1-SNAPSHOT.jar &
 nohup java -jar /home/ec2-user/deploy/showDeliveryReports-0.0.1-SNAPSHOT.jar &
 nohup java -jar /home/ec2-user/deploy/showStockReports-0.0.1-SNAPSHOT.jar &
+echo "working on starting the jars"
 exit
