4428a69c63ae45f47315cab295b97bbb8e89c619
gabrielk <mail@gabrielkovacs.net>
2019-12-30 17:46:04 +0100

    updated start_jars

--- scripts/start_jars.sh
+++ scripts/start_jars.sh
@@ -2,4 +2,3 @@
 nohup java -jar /home/ec2-user/deploy/orderProductsAndReceiveOrderedProducts-0.0.1-SNAPSHOT.jar &
 nohup java -jar /home/ec2-user/deploy/showDeliveryReports-0.0.1-SNAPSHOT.jar &
 nohup java -jar /home/ec2-user/deploy/showStockReports-0.0.1-SNAPSHOT.jar &
-
