485dcc683e95540c533c75a3c096b8dc99d313ad
gabrielk <mail@gabrielkovacs.net>
2019-12-30 21:00:42 +0100

    updated start jars

--- scripts/start_jars.sh
+++ scripts/start_jars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-java -jar /home/ec2-user/deploy/orderProductsAndReceiveOrderedProducts-0.0.1-SNAPSHOT.jar & 
-java -jar /home/ec2-user/deploy/showDeliveryReports-0.0.1-SNAPSHOT.jar &
-java -jar /home/ec2-user/deploy/showStockReports-0.0.1-SNAPSHOT.jar &
+java -jar /home/ec2-user/deploy/orderProductsAndReceiveOrderedProducts-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 & 
+java -jar /home/ec2-user/deploy/showDeliveryReports-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+java -jar /home/ec2-user/deploy/showStockReports-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
