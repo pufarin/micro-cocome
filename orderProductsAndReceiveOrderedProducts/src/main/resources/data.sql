012595ead9a3feb5ae6121794e9ed6c88a71bfad
gabrielk <mail@gabrielkovacs.net>
2020-04-29 13:56:34 +0200

    migrated all services from in memory h2 to postgresql DB

--- orderProductsAndReceiveOrderedProducts/src/main/resources/data.sql
+++ orderProductsAndReceiveOrderedProducts/src/main/resources/data.sql
@@ -1,17 +1,17 @@
-/*
-insert into productorder (id, delivery_date, ordering_date, store_id) values (5, '2019-10-06' ,'2019-10-04' , 200);
-insert into productorder (id, delivery_date, ordering_date, store_id) values (6, '2019-10-07', '2019-10-04', 200);
-insert into productorder (id, delivery_date, ordering_date, store_id) values (7, '2019-10-08', '2019-10-04', 200);
-insert into productorder (id, delivery_date, ordering_date, store_id) values (8, '2019-10-06', '2019-10-04', 201);
-insert into productorder (id, delivery_date, ordering_date, store_id) values (9, '2019-10-07', '2019-10-04', 201);
-insert into productorder (id, delivery_date, ordering_date, store_id) values (10, '2019-10-08', '2019-10-04', 201);
-insert into productorder (id, delivery_date, ordering_date, store_id) values (11, '2019-10-09', '2019-10-04', 200);
 
-insert into orderentry (id, amount, product_id, product_order_id) values (106, 10, 100, 5);
-insert into orderentry (id, amount, product_id, product_order_id) values (107, 20, 101, 6);
-insert into orderentry (id, amount, product_id, product_order_id) values (108, 40, 102, 7);
-insert into orderentry (id, amount, product_id, product_order_id) values (109, 99, 100, 8);
-insert into orderentry (id, amount, product_id, product_order_id) values (110, 98, 101, 9);
-insert into orderentry (id, amount, product_id, product_order_id) values (111, 97, 102, 10);
-insert into orderentry (id, amount, product_id, product_order_id) values (112, 100, 103, 11);
-*/
+insert into public.productorder (id, delivery_date, ordering_date, store_id) values (5, '2019-10-06' ,'2019-10-04' , 200);
+insert into public.productorder (id, delivery_date, ordering_date, store_id) values (6, '2019-10-07', '2019-10-04', 200);
+insert into public.productorder (id, delivery_date, ordering_date, store_id) values (7, '2019-10-08', '2019-10-04', 200);
+insert into public.productorder (id, delivery_date, ordering_date, store_id) values (8, '2019-10-06', '2019-10-04', 201);
+insert into public.productorder (id, delivery_date, ordering_date, store_id) values (9, '2019-10-07', '2019-10-04', 201);
+insert into public.productorder (id, delivery_date, ordering_date, store_id) values (10, '2019-10-08', '2019-10-04', 201);
+insert into public.productorder (id, delivery_date, ordering_date, store_id) values (11, '2019-10-09', '2019-10-04', 200);
+
+insert into public.orderentry (id, amount, product_id, product_order_id) values (106, 10, 100, 5);
+insert into public.orderentry (id, amount, product_id, product_order_id) values (107, 20, 101, 6);
+insert into public.orderentry (id, amount, product_id, product_order_id) values (108, 40, 102, 7);
+insert into public.orderentry (id, amount, product_id, product_order_id) values (109, 99, 100, 8);
+insert into public.orderentry (id, amount, product_id, product_order_id) values (110, 98, 101, 9);
+insert into public.orderentry (id, amount, product_id, product_order_id) values (111, 97, 102, 10);
+insert into public.orderentry (id, amount, product_id, product_order_id) values (112, 100, 103, 11);
+
