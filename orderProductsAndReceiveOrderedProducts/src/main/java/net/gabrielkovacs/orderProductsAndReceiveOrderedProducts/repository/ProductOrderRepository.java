d742989cd7017b26e67dfc46bea4032ebcec37f3
gabrielk <mail@gabrielkovacs.net>
2020-05-07 14:02:20 +0200

    sql query bug

--- orderProductsAndReceiveOrderedProducts/src/main/java/net/gabrielkovacs/orderProductsAndReceiveOrderedProducts/repository/ProductOrderRepository.java
+++ orderProductsAndReceiveOrderedProducts/src/main/java/net/gabrielkovacs/orderProductsAndReceiveOrderedProducts/repository/ProductOrderRepository.java
@@ -17,7 +17,7 @@ public interface ProductOrderRepository extends JpaRepository<ProductOrder, Long
     @Query(value = "select * from productorder po where po.id = (select product_order_id from orderentry o where o.id=?1)", nativeQuery = true)
     Optional<ProductOrder> getProductOrderByOrderEntryId(long orderEntryId);
 
-    @Query(value = "select oe.product_id as productId , po.delivery_date - po.ordering_date as nrDays from productorder po join orderentry oe " +
+    @Query(value = "select oe.product_id as productId , (po.delivery_date - po.ordering_date) as nrDays from productorder po join orderentry oe " +
                     "on oe.product_order_id = po.id where oe.product_id in (?1)", nativeQuery = true)
     List<ProductDeliveryDuration> getNrDaysPerProductDelivery(List<Long> productsId);
 
