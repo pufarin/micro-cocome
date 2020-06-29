8207d690f8d7c415329fa3800f8b23de84cd0a16
pufarin <pufarin@users.noreply.github.com>
2020-06-29 09:48:21 +0200

    changed baseUri value

--- orderProductsAndReceiveOrderedProducts/src/main/java/net/gabrielkovacs/orderProductsAndReceiveOrderedProducts/services/OrderProductService.java
+++ orderProductsAndReceiveOrderedProducts/src/main/java/net/gabrielkovacs/orderProductsAndReceiveOrderedProducts/services/OrderProductService.java
@@ -25,7 +25,7 @@ public class OrderProductService {
     private ProductOrderRepository productOrderRepository;
     private OrderEntryRepository orderEntryRepository;
     
-    private final String baseUri = "http://localhost:8085";
+    private final String baseUri = "http://swa-kovacs-vm2.cs.univie.ac.at/sr";
     private final String getStockItemByStoreIdAnsProductId = "/stockitem?storeId={storeId}&productId={productId}";
     private final String updateStockItemAmount = "/stockitem/{stockItemId}";
 
